58cb2736c57998a81835ad00032457c1619eaa47
izufarahiyah <84302536+izufarahiyah@users.noreply.github.com>
2021-06-03 15:27:26 +0800

    Update foodHome.php

--- ApplicationLayer/ManageFoodInterface/foodHome.php
+++ ApplicationLayer/ManageFoodInterface/foodHome.php
@@ -2,13 +2,6 @@
 session_start();
     // $_SESSION = [];
 
-require_once '../../BusinessServiceLayer/controller/foodController.php';
-require_once '../../BusinessServiceLayer/controller/cartController.php';
-$food = new foodController();
-$cart = new cartController();
-$data = $food->viewAll(); 
-$view_variable = 'a string here';
-
 // require_once '../controller/customerController.php';
 if (!isset($_SESSION['username'])) {
   $message = "You must log in first";
@@ -32,18 +25,18 @@ if (!isset($_SESSION['username'])) {
     ?>
 
 
-  <div style="background-image: url('../../images/foodList.jpg');">
+  <div style="background-image: url('../../images/You Stay at Home.png');">
 
     <div class="ht__bradcaump__wrap d-flex align-items-center">
       <div class="container">
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="bradcaump__inner text-center">
-              <h2 class="bradcaump-title">Food Delivery</h2>
+              <h2 class="bradcaump-title">Food's Homepage</h2>
               <nav class="bradcaump-inner">
                 <a class="breadcrumb-item" href="../../ApplicationLayer/ManageLoginInterface/index.php">Home</a>
                 <span class="brd-separetor"><i class="zmdi zmdi-long-arrow-right"></i></span>
-                <span class="breadcrumb-item active">Food Delivery</span>
+                <span class="breadcrumb-item active">Food's Homepage</span>
               </nav>
             </div>
           </div>
@@ -60,33 +53,25 @@ if (!isset($_SESSION['username'])) {
 <!-- Start Service Area -->
 
 <section class="type__category__area bg--white section-padding">
- <div style="background-image: url('../../images/goodMenu.jpg');">
+ <!--<div style="background-image: url('../../images/.jpg');"> -->
 <div class="wrapper wrapper--w790">
   <div class="card card-5">
     <div class="card-heading">
-      <h2 class="title">Food Delivery Services</h2>
+      <h2 class="title">Choose your Option</h2>
     </div>
     <div class="card-body">
-      <?php
-      if ($_SESSION['usergroup'] == 1) {
-        ?>
-        <div>
-          <center>
-            <button class="btn btn--radius-2 btn--red"> <a href="foodList.php">Food List</a></button>
-           
-          </center>
-        </div>
+     
         <?php
-      } elseif ($_SESSION['usergroup'] == 2) { ?>
+      if ($_SESSION['usergroup'] == 2) { ?>
         <div>
           <center>
-            <button class="btn btn--radius-2 btn--red"> <a href="addFood.php">Add Food</a></button>
+            <button class="btn btn--radius-2 btn--red"> <a href="addFood.php">Add New Menu</a></button>
           </center>
         </div>
         <br></br>
         <div>
           <center>
-            <button class="btn btn--radius-2 btn--red"> <a href="foodList.php">Manage Food</a></button>
+            <button class="btn btn--radius-2 btn--red"> <a href="foodList.php">Manage Menu</a></button>
           </center>
         </div> 
         <?php
@@ -105,68 +90,6 @@ if (!isset($_SESSION['username'])) {
 </div>
 
 </section>
-
-<section class="type__category__area bg--white section-padding">
- <div style="background-image: url('../../images/goodMenu.jpg');">
-  <div class="wrapper wrapper--w790">
-    <div class="card card-5">
-      <div class="card-heading">
-        <h2 class="title">Food List</h2>
-      </div>
-      <div class="card-body">
-  <center>
-    <!-- <div class="content_resize2"> -->
-      <!-- <center> -->
-      <table>
-            <thead>
-            <th>Name</th>
-            <th>Image</th>
-            <th>Price</th>
-            <th></th>
-            <th>Action</th>
-            </thead>
-            <?php
-            $i = 1;
-            foreach($data as $row){
-              $image =  $row['food_image'];
-              $isrc = "../../images/";
-
-               echo "<tr>"
-                . "<td>".$row['food_name']."</td>"
-                . "<td><img src=\"" .$isrc. $row['food_image'] . "\" height=\"130\" width=\"150\"> </td>"
-                ."<td>RM".$row['food_price']."</td>";                         
-                       // . "<td>".$row['food_price']."</td>";
-               ?>
-                 <td></td>
-            <td><form action="" method="POST">
-              <?php
-              if ($_SESSION['usergroup'] == 1) {
-                  ?>
-               <button class="btn btn--radius-2 btn--red" input type="button" name="view" value="View" onclick="location.href='../../ApplicationLayer/ManageFoodInterface/view.php?food_id=<?=$end['food_id']?>'">View</button>
-               <br></br>
-              <input type="hidden" name="name" value="<?=$row['food_name']?>">
-              <input type="hidden" name="price" value="<?=$row['food_price']?>">
-              <input type="hidden" name="image" value="<?=$row['food_image']?>">
-              <input type="hidden" name="quantity" value="1">
-              <button class="btn btn--radius-2 btn--red" type="submit" name="buy" value="BUY">Buy</button>
-               <br></br>
-             
-             
-              <?php
-            }?>
-
-                </form></td>
-              <?php
-              $i++;
-             echo "</tr>";
-            }
-            ?>
-        </table>
-      </center>
-      </div>
-    </center>
-</section>
-
 
 
 
